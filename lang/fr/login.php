7e1dce5c5377cc52962d53ce769058a8000e53ef
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-11-22 12:09:56 +0100

    translate login page to fr

---
+++ lang/fr/login.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'Email' => 'Email',
+    'Password' => 'Mot de passe',
+    'Remember me' => 'Rester connecté',
+    'Forgot your password?' => 'Mot de passe oublié',
+    'Log in' => 'Se connecter',
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label dusk="email__label" for="email" :value="__('Email')" />
+            <x-input-label dusk="email__label" for="email" :value="__('login.Email')" />
             <x-text-input dusk="email__input"  id="email" class="block mt-1 w-full" type="text" name="email" :value="old('email')"/>
 
             <label for="email"></label>
@@ -16,7 +16,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label dusk="password__label" for="password" :value="__('Password')" />
+            <x-input-label dusk="password__label" for="password" :value="__('login.Password')" />
 
             <x-text-input dusk="password__input" id="password" class="block mt-1 w-full"
                             type="password"
@@ -30,19 +30,19 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <span class="ms-2 text-sm text-gray-600">{{ __('login.Remember me') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                    {{ __('login.Forgot your password?') }}
                 </a>
             @endif
 
             <x-primary-button dusk="login__button" class="ms-3">
-                {{ __('Log in') }}
+                {{ __('login.Log in') }}
             </x-primary-button>
         </div>
     </form>
