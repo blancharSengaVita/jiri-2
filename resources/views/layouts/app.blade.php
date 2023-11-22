9fc734d2e4c66cb3b0aa59fa768eb775a073ea91
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-11-22 21:56:31 +0100

    make html of app-layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,33 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ $title . ' - ' . config('app.name', 'Laravel')}}</title>
+    <title>{{ $title . ' - ' . config('app.name', 'Laravel')}}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Scripts -->
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+</head>
 
-        <!-- Scripts -->
-        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+<body class="">
+<!-- Page Content -->
+<h1 class="sro"> Dashboard </h1>
+<h2 class="sro"> menu de navigation principale  </h2>
+<main class="app-layout">
+    <div class="app-layout__container">
+        <p class="app-layout__logo">jiri</p>
+        <p> menu </p>
+        @include('layouts.navigation')
+    </div>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+    <div class="app-layout__container">
+        <x-responsive-nav-link :href="route('profile.edit')">
+            {{ __('Profile') }}
+        </x-responsive-nav-link>
+        {{ $slot }}
+    </div>
+</main>
+</body>
 </html>
