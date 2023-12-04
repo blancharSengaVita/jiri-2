1da43ba093fa2121c196556c020d13ec44affdb0
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-12-04 20:26:40 +0100

    add gate for jiriControllers

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\Jiri;
+use App\Models\Note;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('handle-note', function (User $user, Jiri $jiri) {
+            return $jiri->user_id === $user->id;
+        });
     }
 }
