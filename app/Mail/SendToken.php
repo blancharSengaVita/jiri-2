3b4404d0964a815b68c87b813c5c0b1b93810959
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2024-01-12 18:35:26 +0100

    add mail controller

---
+++ app/Mail/SendToken.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Attendance;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendToken extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected string $token;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: new Address(env('MAIL_FROM_ADDRESS', 'hello@example.com'), env('MAIL_FROM_NAME', 'example')),
+            subject: 'Envoi du token pour le jiri',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.send-token',
+            with: [
+                'token' => $this->token,
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
